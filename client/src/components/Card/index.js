1ab74e9900a9372fdaa56ad27458e3392acb630d
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-08-20 14:19:30 -0400

    added card component.

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -7,13 +7,14 @@ import "./style.css";
 
 function Card(props) {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none"
-      }}
-    >
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
+  </div>
   );
 }
 
