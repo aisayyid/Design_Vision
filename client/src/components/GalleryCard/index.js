a08c5374555572a3e3f74f08066ff12efea915b7
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-08-22 16:33:12 -0400

    added delete route on the server side.

--- client/src/components/GalleryCard/index.js
+++ client/src/components/GalleryCard/index.js
@@ -4,14 +4,31 @@ import "./style.css";
 
 function GalleryCard({ image }) {
 
+  const user = useSelector(state => state.auth.currentUser);
+  //function
+  console.log("this is the user" , user)
+
+  function imageDelete(imageName){
+    //delete method
+    axios.delete("/gallerydelete",
+    { //using id
+     user: user._id,
+    //delete image from gallery array
+      gallery: imageName
+    }).then(data => {
+      console.log(data)
+    })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className = "col-3">
     <div className="card" style={{width: "18rem;"}}>
     <img src={`./uploads/${image}`} className="card-img-top" alt="..."/>
     <div className="card-body">
-    <p>This is a card</p>
+ 
     </div>
-    <button type="button" className="btn btn-primary btn-sm">Remove from Gallery</button>
+    <button type="button" onClick = {(e) => imageDelete(image.imageName)} className="btn btn-primary btn-sm">Remove from Gallery</button>
     </div>
   </div>
   );
