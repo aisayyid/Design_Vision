3f7a03e77291cdb5f130c51f4815834733e46fe3
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-09-10 17:39:13 -0400

    removed gallery in res authController.

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ module.exports = {
                             firstname,
                             lastname,
                             email,
-                            gallery
+                          
                         })
                     })
 
